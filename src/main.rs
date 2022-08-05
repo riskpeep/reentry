7d61ff28cf45a2c2684a46d9aa635447589b075b
Robert Kerr <rskerr@gmail.com>
2022-08-05 10:52:34 -0400

    1 - First Steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Reentry. A space adventure.");
+    println!("");
+    println!("You awake in darkness with a pounding headache.");
+    println!("An alarm is beeping loudly. This doesn't help your headache.");
+    println!("");
+    println!("Bye!");
 }
